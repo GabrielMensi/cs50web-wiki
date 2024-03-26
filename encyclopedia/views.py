98b1dbd0dccf9c85cf39eb3e4ebf1362c634316e
gabrielmensi <gabriel.mensi7@yahoo.com.ar>
2024-03-25 23:14:34 -0300

    Agregada función para convertir Markdown a HTML en views.py

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 
 from . import util
+from markdown2 import Markdown
+
+
+def convert_md_to_html(entry):
+    print("convert entry:" + entry)
+    markdowner = Markdown()
+    if entry:
+        return markdowner.convert(entry)
+    else:
+        return None
 
 
 def index(request):
@@ -8,3 +18,16 @@ def index(request):
         "entries": util.list_entries()
     })
 
+
+def entry(request, title):
+    entry = util.get_entry(title)
+    html_content = convert_md_to_html(entry)
+    if html_content:
+        return render(request, "encyclopedia/entry.html", {
+            "title": title,
+            "content": html_content
+        })
+    else:
+        return render(request, "encyclopedia/error.html", {
+            "message": "The requested page was not found."
+        })
